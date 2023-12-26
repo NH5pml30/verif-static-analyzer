ce70817760da5814b89e2c76267c9bea02076f4f
nh5@wsl <holiavin.nikolai.nh5@gmail.com>
2023-12-27 02:16:03 +0300

    Change deps

--- configure.sh
+++ configure.sh
@@ -3,8 +3,8 @@ rm -rf build && mkdir build && \
   cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DCMAKE_CXX_COMPILER=clang++-14 \
-  -DCMAKE_C_COMPILER=clang-14 \
+  -DCMAKE_CXX_COMPILER=clang++-15 \
+  -DCMAKE_C_COMPILER=clang-15 \
   -S. \
   -B./build \
   -G Ninja
